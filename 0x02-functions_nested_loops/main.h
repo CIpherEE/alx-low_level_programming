985b8d90181340aaef570ffbd1fa6650ce7a6db4
Praise Balogun <praisebalogun4@gmail.com>
2022-09-14 07:24:17 -0700

    Help

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,8 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _MAIN_H_INCLUDED
+#define _MAIN_H_INCLUDED
 
-#include <stdio.h>
 
-int_putchar(char c);
+int_putchar(char);
 void print_aplhabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,7 +12,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
+void print_times_tables(int n);
+void num(int r, int n);
 
 #endif
