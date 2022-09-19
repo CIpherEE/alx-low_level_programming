f6f5c9fded23f35c06140a43eb0f598bbc7595ea
Praise Balogun <praisebalogun4@gmail.com>
2022-09-19 16:46:13 -0700

    Used dereferancing to change int value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_9 - Resets the valuse of any int through it's pointer
+ * @n: pointer to int
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+New project on poimters arrays and strings
